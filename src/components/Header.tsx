b6fa21be2322775dd13b79282b24ccf7c1ab63c9
Raihan <raihanhn20@gmail.com>
2023-11-25 01:24:36 +0600

    logout button & functionality added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,28 +1,34 @@
+import { getCurrentUser } from "@/lib/session";
 import Link from "next/link";
 import React from "react";
+import ButtonLogout from "./ButtonLogout";
 
-const Header = () => {
+const Header = async () => {
+  const user = await getCurrentUser();
   return (
     <header className="bg-blue-500 p-4">
       <nav className="flex justify-between items-center max-w-4xl mx-auto">
         <Link href={"/"} className="text-white text-2xl font-bold">
           My Blogs
         </Link>
-
         <ul className="flex space-x-4">
           <li>
             <Link href={"/blogs"} className="text-white hover:underline">
               Blogs
             </Link>
           </li>
-          <li>
-            <Link
-              href={"/api/auth/signin"}
-              className="text-white hover:underline"
-            >
-              login
-            </Link>
-          </li>
+          {user?.name ? (
+            <ButtonLogout />
+          ) : (
+            <li>
+              <Link
+                href={"/api/auth/signin"}
+                className="text-white hover:underline"
+              >
+                login
+              </Link>
+            </li>
+          )}
         </ul>
       </nav>
     </header>
